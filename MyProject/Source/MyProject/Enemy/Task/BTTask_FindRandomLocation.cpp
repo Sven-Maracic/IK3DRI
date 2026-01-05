cd37d598d83e1f05e876492eb3e0f03147c36a35
Sven-Maracic <161666654+Sven-Maracic@users.noreply.github.com>
2026-01-05 22:41:11 +0100

    Base class setup and Patrolling
    #Added base classes and first enemy class
    - Added Enemy base class
    - Added EnemyGround base class
    - Added BP_EnemySlime class
    
    #Added simple AI with Blackboard and Behavior tree for grounded enemies
    - Added BB_Ground blackboard
    - Added BT_Ground behaviour tree (NOT FINAL)
    - Added FindRandomLocation task to find random location on patrol
    - Added AI Controller (not sure what this does yet, everything works but AIC_EnemyGround is left blank?)
    
    ##Added 2 test levels to debug
    
    ##Updated .gitignore to include Binaries, Saved, Intermediate and .idea folders.
    
    #Comments
    Currently the behavior tree is checking if a path is valid and is choosing a new path based on if it is. The check it's doing is part of a decorator called "Does Path Exist". It is determening if path is valid using Raycast 2d (the fastest option). This does result in the path finding code to run a couple times before it finds a suitable spot. THIS CAN BE REPLACED WITH THE SLOWEST OPTION (forgot the name) BUT IDK IF ITS FASTER LIKE THIS OR THAT.
    
    Still don't know what the AI Controller does.
    
    NEXT UPDATE
    Add LoS logic (cone of vision), run to player if spotted

---
+++ MyProject/Source/MyProject/Enemy/Task/BTTask_FindRandomLocation.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemy/Task/BTTask_FindRandomLocation.h"
+
+#include "NavigationSystem.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Enemy/EnemyGround/AIC_EnemyGround.h"
+#include "NavFilters/NavigationQueryFilter.h"
+
+UBTTask_FindRandomLocation::UBTTask_FindRandomLocation()
+{
+	NodeName = "Find Random Location in Radius on NavMesh";
+}
+
+EBTNodeResult::Type UBTTask_FindRandomLocation::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	//get owner of script and cast it to AI controller base class
+	if (AAIC_Base* const aiController = Cast<AAIC_Base>(OwnerComp.GetAIOwner()))
+	{
+		if (APawn* const enemy = aiController->GetPawn()) //cast aiController object as Pawn
+		{
+			const FVector origin = enemy->GetActorLocation(); //get enemy location
+			
+			//UNavigationSystem implements NavMesh detection in our script
+			if (UNavigationSystemV1* const navSys = UNavigationSystemV1::GetCurrent(GetWorld()))
+			{
+				FNavLocation navLocation;
+				//GetRandomPointInNavigableRadius -> finds a random point on navmesh within SearchRadius starting at Origin, saves it in NavLocation
+				if (navSys->GetRandomPointInNavigableRadius(origin, SearchRadius, navLocation))
+				{
+					//save random location to blackboard
+					OwnerComp.GetBlackboardComponent()->SetValueAsVector("TargetLocation", navLocation.Location);
+				}
+				//debug, draw where enemy is trying to move to
+				DrawDebugSphere(GetWorld(), navLocation, 10.0f, 32, FColor::Cyan, false, 3, 0, 2.0f);
+			}
+			return EBTNodeResult::Succeeded; //return success
+		}
+	}
+	return EBTNodeResult::Failed; //if casting failed, return fail
+}
